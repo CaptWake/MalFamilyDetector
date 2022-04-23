cc18cf47ca5f141f509126fe5b1ee7f61447358f
CaptWake <50182623+CaptWake@users.noreply.github.com>
2022-04-23 20:28:02 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,8 @@
 """
     TO-DO:
-        - Aggiungere la possibilità di salvare il dataset generato in npz o csv -> metodo di PEPreprocessing
-        - Aggiungere la possibilità di fare features selection
+        - Scrivere il codice per esportare in csv o json o npz 
+        - aggiungere export di y
+        - refactoring
 """
 import pandas as pd
 import numpy as np
@@ -11,6 +12,7 @@ import utils
 import file_parser
 import pathlib
 import hashlib
+import json
 
 from collections import OrderedDict 
 from sklearn.impute import SimpleImputer
@@ -36,7 +38,7 @@ class PEPreprocessingBuilder:
         return tpe
     
     def from_existing_raw_dataset(self, X_file):
-        X = PEDataset().load_raw_from_file(X_file, y_file)
+        X = PEDataset().load_raw_from_file(X_file)
         rpe = RawPEPreprocessing()
         rpe.set_dataset(X)
         return rpe
@@ -49,7 +51,6 @@ class PEPreprocessingBuilder:
 
 
 class PEPreprocessing:
-    
     dataset = None
     
     def set_dataset(self, X):
@@ -57,6 +58,8 @@ class PEPreprocessing:
     
     def get_dataset(self):
         return self.dataset
+    
+    #def export_dataset(self, filename):   
     
     #@abstractmethod
     def data_cleansing(self):
@@ -69,7 +72,7 @@ class PEPreprocessing:
     #@abstractmethod
     def representation_transformation(self):
         pass
-
+    
     ''' 
     Scale features to a standard range so that all values are within the new range of 0 and 1. 
     Useful for models that use a weighted sum of input variables i.e SVM, MLP, KNN
@@ -81,37 +84,47 @@ class PEPreprocessing:
     
         
 class RawPEPreprocessing(PEPreprocessing):
+    def get_X(self):
+        return np.vstack(self.dataset['features'])
+    
+    def export_dataset(self, filename):
+        exported = self.dataset.to_json(orient="records")
+        with open(filename, 'w+') as f:
+            json.dump(json.loads(exported), f)
+    
     def data_cleansing(self):
         self.dataset = pd.DataFrame(self.dataset, columns = ['sha256', 'features']).drop_duplicates(subset=['sha256'])
-        self.dataset = np.vstack(self.dataset['features'])
-        return self.dataset
+        return self
     
     def representation_transformation(self):
-        self.dataset = self._normalize(self.dataset)
-        return self.dataset
+        features = np.vstack(self.dataset['features'])
+        self.dataset['features'] = self._normalize(features).tolist()
+        return self
 
     
 class TrainTestPEPreprocessing(PEPreprocessing):
-
+    
+    def get_X_y(self):
+        return np.vstack(self.dataset['X']['features']), self.dataset['y']
+    
+    def export_dataset(self, filename):
+        self.dataset['X'] = self.dataset['X'].to_json(orient="records")
+        with open(filename, 'w+') as f:
+            json.dump(json.loads(self.dataset['X']), f)
+    
     def data_cleansing(self):
         # removes duplicates
-        hash_list = list(OrderedDict.fromkeys([row['sha256'] for row in self.dataset['X']]))
-        self.dataset['X'] = pd.DataFrame(self.dataset['X']).drop_duplicates(subset=['sha256'])
-        # non so se ci vada o meno testiamo tra poco
-        #self.dataset['X'] = self.dataset['X'][(self.dataset['X']['sha256'].isin(hash_list))]
-        self.dataset['y'] = self.dataset['y'][(self.dataset['y']['sha'].isin(hash_list))]
-        self.dataset['X'] = self.dataset['X'].drop(columns=['sha256'])
-        # transform dataframe to numpy
-        # -----
-        self.dataset['X'] = np.vstack(self.dataset['X']['features'].to_numpy())
-        # ---
-        self.dataset['y'] = list((self.dataset['y'].drop(columns=['sha', 'timestamp']))['family'])
-        return self.dataset['X'], self.dataset['y']
-        
+        self.dataset['X'] = pd.DataFrame(self.dataset['X'], columns = ['sha256', 'features']).drop_duplicates(subset=['sha256'])
+        self.dataset['y'] = self.dataset['y'][(self.dataset['y']['sha'].isin(self.dataset['X']['sha256']))]
+        # added now
+        return self
+    
     def representation_transformation(self):
-        self.dataset['X'] = self._normalize(self.dataset['X'])
-        self.dataset['y'] = self.__prepare_targets(self.dataset['y'])
-        return self.dataset['X'], self.dataset['y']
+        features = np.vstack(self.dataset['X']['features'])
+        self.dataset['X']['features'] = self._normalize(features).tolist()
+        self.dataset['y']['family'] = self.__prepare_targets(list(self.dataset['y']['family']))
+        print(self.dataset['y'])
+        return self
 
     ''' 
     Transform training set to continuous labels using ordinal encoding cause the 
@@ -129,7 +142,7 @@ class TrainTestPEPreprocessing(PEPreprocessing):
         for i in range(len(raw)):
             mapping[raw[i]] = enc[i]  # mapping: real label -> converted label
             inv_mapping[str(enc[i])] = raw[i] # inv_mapping: converted label -> real label
-        print(inv_mapping)
+        #print(inv_mapping)
         #logging.debug(f'LabelEncoder mapping: {mapping}')
         #logging.debug(f'after relabeling training: {Counter(y_train)}')
         #utils.dump_json(inv_mapping, setting_data_folder, f'top_{families_cnt}_label_mapping.json')
@@ -152,6 +165,7 @@ class PEDataset:
         files = pathlib.Path(dir_name).glob('*')
         for file in files:
             X.append({'sha256': calculate_sha256(file), 'features': self.__extract_features_from_binary(file, features_file)})
+        X = pd.DataFrame(X) 
         y = self.__extract_dataset_from_file(y_file)
         return X, y
     
@@ -164,9 +178,6 @@ class PEDataset:
         file_data = open(binary_name, 'rb').read()
         extractor = ember.features.PEFeatureExtractor(2, features_file=features_file)
         features = np.array(extractor.feature_vector(file_data))
-        #X = features[:,np.newaxis]
-        #print(X.T.shape)
-        #return X.T
         return features
 
     def __extract_dataset_from_file(self, file):
@@ -176,35 +187,25 @@ class PEDataset:
             df = file_parser.JSONcreator().create_parser().parse(file)
         elif(ext == '.csv'):
             df = file_parser.CSVcreator().create_parser().parse(file)
+        #elif(ext == '.npz'):
+        #    df = pd.DataFrame(np.load(file), columns=['X']
         return df
     
     
 def main():
-    #pe = PEPreprocessingBuilder().new_train_test_dataset('/home/students/derosa/test/', '/home/students/derosa/bodmas/bodmas_metadata.csv', '/home/students/derosa/config.json')
-    pe = PEPreprocessingBuilder().new_raw_dataset('/home/students/derosa/test/', '/home/students/derosa/config.json')
-    print(pe.data_cleansing())
-    X = pe.representation_transformation()
+    #PEPreprocessingBuilder().from_existing_train_test_dataset('/home/students/derosa/test/', '/home/students/derosa/bodmas/bodmas_metadata.csv', '/home/students/derosa/config.json')
+    pe1 = PEPreprocessingBuilder().new_train_test_dataset('/home/students/derosa/test/', '/home/students/derosa/bodmas/bodmas_metadata.csv', '/home/students/derosa/config.json')
+    pe2 = PEPreprocessingBuilder().from_existing_raw_dataset('/home/students/derosa/bodmas/BODMAS/code/bodmas/prova.json')
+    X = pe2.data_cleansing().representation_transformation().get_X()
+    #X = pe2.data_cleansing().get_X()
     lgbm_model = lgb.Booster(model_file=MODEL_FOLDER + 'gbdt_bluehex_families_238_r10.txt')
     print(predict(lgbm_model, 'gbdt', X, DATA_FOLDER + 'top_238_label_mapping.json'))
-
-
-
-'''
-def normalize(X_train, X_test_list):
-    scaler = MinMaxScaler()
-
-    X_train_scale = scaler.fit_transform(X_train)
-    logging.debug(f'X_train_scale: {X_train_scale.shape}')
-
-    X_test_scale_list = []
-    logging.debug(f'X_test_list: {len(X_test_list)}, X_test_list[0]: {X_test_list[0].shape}')
-    for X_test in X_test_list:
-        X_test_scale = scaler.transform(X_test)
-        X_test_scale_list.append(X_test_scale)
-
-    return X_train_scale, X_test_scale_list
-'''
-
+    #print(type(pe1.data_cleansing().representation_transformation().get_dataset()))
+    #print(pe1.get_X_y())
+    #pe2.export_dataset('prova2.json')
+    pe1.data_cleansing().representation_transformation().export_dataset('prova4.json')
+    
+    
 # mettere un parametro di mapping labels
 def predict(model, clf, X, label_mapping_file):
     if clf == 'rf':
